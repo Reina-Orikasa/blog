d7aac4e319d2c752ed4891bc4231fe26412474c1
Reina-Orikasa <tammichael76@gmail.com>
2020-08-07 02:40:28 -0700

    Change how navigation works

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,12 @@ function scrollToId(id) {
   var elementPosition = document.getElementById(id).offsetTop;
   window.scrollTo(0, elementPosition - 75);
 }
+
+function hideDiv() {
+  var id = document.querySelector('.hidden');
+  if (id.style.display === 'none') {
+    id.style.display = 'block';
+  } else {
+    id.style.display = 'none';
+  }
+}
